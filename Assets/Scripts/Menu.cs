d9c3b92768fbed2b89422b43a7cc97a59d6edfeb
wojwesoly <wojwesoly@gmail.com>
2021-06-27 11:27:10 +0200

    Added exit button

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public Button btn;
+    public Button btn1;
+    void Start()
+    {
+        Button StartBtn = btn.GetComponent<Button>();
+        Button ExitBtn = btn1.GetComponent<Button>();
+        StartBtn.onClick.AddListener(Load);
+        ExitBtn.onClick.AddListener(Exit);
+    }
+    public void Load()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
